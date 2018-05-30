97b6b0dcefec32ed87f6ffa29f9825876a1a1c2f
Ai Nguyen <ainguyenkaka@gmail.com>
2018-05-30 11:54:14 +0800

    rename MessageStore to MessageModule

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import MessageStore from './MessageStore'
+import MessageModule from './MessageModule'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    message: MessageStore
+    message: MessageModule
   },
   state: {
   },
